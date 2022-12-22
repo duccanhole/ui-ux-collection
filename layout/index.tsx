5b426a7bdb00b98a3dacf29a6827c7dc9afe291c
duccanhole <duccanhole@gmail.com>
2022-12-22 22:58:03 +0700

    add UI, style button

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,10 +1,37 @@
-import { Button } from "@nextui-org/react";
-import React from "react";
+import { Button, Modal } from "@nextui-org/react";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import React, { useState } from "react";
 
 export default function DefaultLayout({ children }: any) {
+  const [modal, setModal] = useState<boolean>(false);
+  const style: React.CSSProperties = {
+    position: "fixed",
+    top: "10px",
+    right: "10px",
+  };
   return (
     <>
-      <Button color="primary">XX</Button> {children}
+      <Button
+        auto
+        shadow
+        color="primary"
+        icon={<BsFillGrid3X3GapFill />}
+        style={style}
+        onClick={() => {
+          alert('click');
+          setModal(true);
+        }}
+      ></Button>
+      <Modal
+        open={modal}
+        onClose={() => {
+          setModal(false);
+        }}
+      >
+        <Modal.Header>Tesst</Modal.Header>
+        <Modal.Body>asdas</Modal.Body>
+      </Modal>
+      {children}
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,20 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { CssBaseline } from "@nextui-org/react";
 import DefaultLayout from "../layout";
+import { NextPage } from "next";
 
-class _Document extends Document {
-  static async getInitialProps(ctx: any) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: React.Children.toArray([initialProps.styles]),
-    };
-  }
+const Document: NextPage<any> = (props) => (
+  <Html lang="en">
+    <Head>{CssBaseline.flush()}</Head>
+    <body>
+      <DefaultLayout>
+        <Main />
+      </DefaultLayout>
+      <NextScript />
+    </body>
+  </Html>
+);
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
-        <body>
-          <DefaultLayout>
-            <Main />
-          </DefaultLayout>
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default Document;
 
-export default _Document;
