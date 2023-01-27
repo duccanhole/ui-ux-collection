462dae9a875ab75df97fe3376332416baf381c09
duccanhole <duccanhole@gmail.com>
2023-01-27 12:13:35 +0700

    update: fix scroll and style chat input

--- components/chat/chat-room/chat-box.tsx
+++ components/chat/chat-room/chat-box.tsx
@@ -1,5 +1,6 @@
-import { Card, Loading, Text } from "@nextui-org/react";
+import { Card, Loading, Text, Input, styled } from "@nextui-org/react";
 import { useEffect, useRef, useState } from "react";
+import { FiSend } from "react-icons/fi";
 import { IConversationDetail } from "../../../models/chat/conversation";
 import ConversationRepo from "../../../repositories/chat/conversation";
 
@@ -8,24 +9,58 @@ interface PropType {
   roomId: number;
 }
 
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const SendBtn = styled("button", {
+  backgroundColor: "#9ca3af",
+  borderRadius: "$rounded",
+  cursor: "pointer",
+  padding: "15%",
+  marginRight: "10px",
+  "&:active": {
+    transform: "translate(10px)",
+  },
+  "&:hover": {
+    backgroundColor: "$primary",
+  },
+});
+
 export default function ChatBox({ lastMess, roomId }: PropType) {
   // declare variable
   const scrollBar = useRef<any>(null);
-  const [prevId, setId] = useState<number>(roomId);
   const [loading, setLoading] = useState<boolean>(false);
-  const chatMessage: IConversationDetail[] =
-    new ConversationRepo().getConversationMessage(lastMess);
-  if (roomId !== prevId) {
-    alert("change");
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-    setId(roomId);
-  }
+  const [chatMessage, setChatMessage] = useState<IConversationDetail[]>([]);
+  const [message, setMessage] = useState<string>("");
+  // first load
   useEffect(() => {
+    async function getMessageData() {
+      setLoading(true);
+      setChatMessage(new ConversationRepo().getConversationMessage(lastMess));
+      await sleep(1500);
+      setLoading(false);
+    }
+    getMessageData();
+  }, [roomId]);
+  // logic
+  const scrollToBottom = () => {
     scrollBar.current.scrollTop = scrollBar.current?.scrollHeight;
-  }, [lastMess])
+  };
+  const onChatMessage = () => {
+    if (message.trim() !== "")
+      setChatMessage((prev) => [
+        ...prev,
+        {
+          from: "me",
+          message,
+        },
+      ]);
+    setMessage("");
+  };
+  useEffect(() => {
+    if (!loading) scrollToBottom();
+  }, [loading, chatMessage]);
   return (
     <>
       <div className="grow overflow-y-auto">
@@ -41,7 +76,10 @@ export default function ChatBox({ lastMess, roomId }: PropType) {
                   key={index}
                   className={item.from === "other" ? "" : "flex justify-end"}
                 >
-                  <Card variant="flat" className="max-w-lg m-2 w-fit">
+                  <Card
+                    variant="flat"
+                    className="max-w-lg m-2 w-fit"
+                  >
                     <Card.Body>
                       <Text>{item.message}</Text>
                     </Card.Body>
@@ -52,7 +90,25 @@ export default function ChatBox({ lastMess, roomId }: PropType) {
           </div>
         )}
       </div>
-      <div>loading: {String(loading)}</div>
+      <div className="py-2">
+        <Input
+          className="bg-white"
+          bordered
+          color="primary"
+          width="100%"
+          contentRightStyling={false}
+          value={message}
+          contentRight={
+            <SendBtn onClick={onChatMessage}>
+              <FiSend />
+            </SendBtn>
+          }
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") onChatMessage();
+          }}
+        />
+      </div>
     </>
   );
 }
